101fad0de90d0a514a80d6d760cd9aa59318ee08
Sindhu M <sindhu.manchukonda@gmail.com>
2016-01-22 19:28:39 -0800

    Update appendAllZerosAtEnd.py

--- Misc/appendAllZerosAtEnd.py
+++ Misc/appendAllZerosAtEnd.py
@@ -8,3 +8,14 @@ for i in range(len(y) - len(substr)):
 if len(y) == len(substr):
     print ([int(i) for i in list(y)])
     print ([int(i) for i in list(substr)])
+
+
+x = [1,9,8, 4, 0, 0, 2, 7, 0, 6, 0]
+count = 0
+for i in x:
+    if i == 0:
+        count += 1
+for i in range(count):
+    x.remove(0)
+    x.append(0)
+print (x)
