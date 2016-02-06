32e8d4c9d7867c9e8becaea52fb21dd587866f3f
Sindhu M <sindhu.manchukonda@gmail.com>
2016-02-05 19:43:21 -0800

    Create triangle.py

---
+++ Misc/triangle.py
@@ -0,0 +1,36 @@
+def revTriangle(n):
+    for j in range(1,n+1):
+        print (" ".join([str(i+1) for i in range(n-j+1)]))
+
+def triangle(n):
+    for j in range(1,n+1):
+        print (" ".join([str(i+1) for i in range(j)]))
+
+def astrict(n):
+    i = 1
+    while i <= n:
+        print (i * "*")
+        i += 1
+    n -= 1
+
+def revAstrict(n):
+    i = 0
+    while i <= n:
+        print ((n-i) * "*")
+        i += 1
+
+
+def astrict(n):
+    i = 1
+    while i <= n:
+        print (i * "*")
+        i += 1
+    n -= 1
+   
+triangle(5)
+print ()
+revTriangle(5)
+print ()
+astrict(5)
+print ()
+revAstrict(5)
