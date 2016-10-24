cab3f56bc1ee1c1daab29ffec32ea65d72174121
Sindhu M <sindhu.manchukonda@gmail.com>
2016-10-24 16:41:33 -0700

    Create EmployeeDetails.py

---
+++ Classes/EmployeeDetails.py
@@ -0,0 +1,19 @@
+class Employees:
+	def __init__(self, name, age, salary):
+		self.name = name
+		self.age = age
+		self.salary = salary
+	
+	def __repr__(self):
+		return "({}, {}, ${})".format(self.name, self.age, self.salary)
+	
+e1 = Employees('Sindhu', 27, 100000)
+e2 = Employees('Hemanth', 29, 150000)
+e3 = Employees('Likith', 24, 90000)
+emplyee = [e1,e2,e3]
+
+def e_sort(emp):
+	return emp.salary
+	
+my_emp = sorted(emplyee, key=e_sort, reverse=True)
+print(my_emp)
